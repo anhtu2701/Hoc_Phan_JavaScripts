6401ee01b08db65f9fe7180d54e3b56ba27f5e3e
htu.dev <tuah.ptit@gmail.com>
2025-06-04 23:28:25 +0700

    JS30_2025_04_06_Thuc hien them animations cho .footer-nav va them moi backToTop button

--- Bai_Tap_Lon_Giua_Ky/assets/js/main.js
+++ Bai_Tap_Lon_Giua_Ky/assets/js/main.js
@@ -1,40 +1,59 @@
-// Slider Animation
-
-const sliderTrack = document.querySelector('.slider-track');
-const sliderItems = document.querySelectorAll('.slider-item');
-const sliderDots = document.querySelectorAll('.slider-dot');
-const prevBtnSlider = document.querySelector('.slider-btn.prev-btn');
-const nextBtnSlider = document.querySelector('.slider-btn.next-btn');
-
-let currentIndex = 0;
-
-function updateSliderAndDot() {
-    sliderTrack.style.transform = `translateX(-${currentIndex * 100}vw)`;
-
-    sliderDots.forEach((sliderDot, index) => {
-        sliderDot.classList.toggle('selected', index === currentIndex)
-    });
-}
-
-function handleBtnClick(direction) {
-    if (direction === 'next' && currentIndex < sliderItems.length - 1) {
-        currentIndex++;
-    } else if (direction === 'prev' && currentIndex > 0) {
-        currentIndex--;
+document.addEventListener('DOMContentLoaded', function () {
+    // Slider Animation
+    const sliderTrack = document.querySelector('.slider-track');
+    const sliderItems = document.querySelectorAll('.slider-item');
+    const sliderDots = document.querySelectorAll('.slider-dot');
+    const prevBtnSlider = document.querySelector('.slider-btn.prev-btn');
+    const nextBtnSlider = document.querySelector('.slider-btn.next-btn');
+    
+    let currentIndex = 0;
+    
+    function updateSliderAndDot() {
+        sliderTrack.style.transform = `translateX(-${currentIndex * 100}vw)`;
+    
+        sliderDots.forEach((sliderDot, index) => {
+            sliderDot.classList.toggle('selected', index === currentIndex)
+        });
     }
-    updateSliderAndDot()
-}
-
-function goToSlide(index) {
-    currentIndex = index;
-    updateSliderAndDot()
-}
-
-updateSliderAndDot();
-
-sliderDots.forEach((sliderDot, index) => {
-    sliderDot.addEventListener('click', () => goToSlide(index))
+    
+    function handleBtnClick(direction) {
+        if (direction === 'next' && currentIndex < sliderItems.length - 1) {
+            currentIndex++;
+        } else if (direction === 'prev' && currentIndex > 0) {
+            currentIndex--;
+        }
+        updateSliderAndDot()
+    }
+    
+    function goToSlide(index) {
+        currentIndex = index;
+        updateSliderAndDot()
+    }
+    
+    updateSliderAndDot();
+    
+    sliderDots.forEach((sliderDot, index) => {
+        sliderDot.addEventListener('click', () => goToSlide(index))
+    });
+    
+    prevBtnSlider.addEventListener('click', () => handleBtnClick('prev'));
+    nextBtnSlider.addEventListener('click', () => handleBtnClick('next'));
+    
+    
+    // Back to top button
+    const backToTopBtn = document.querySelector('.back-to-top');
+    window.addEventListener('scroll', () => {
+        if (window.pageYOffset > 300) {
+            backToTopBtn.classList.add('show');
+        } else {
+            backToTopBtn.classList.remove('show');
+        }
+    });
+    
+    backToTopBtn.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    });
 });
-
-prevBtnSlider.addEventListener('click', () => handleBtnClick('prev'));
-nextBtnSlider.addEventListener('click', () => handleBtnClick('next'));
