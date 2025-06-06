e2756ff2f54338cdc2a95ac924f14aa2c72bed1c
htu.dev <tuah.ptit@gmail.com>
2025-06-06 15:53:38 +0700

    Update Code 2025_06_06

---
+++ 2025_06_06/ThucHanhTrenLop/date.js
@@ -0,0 +1,16 @@
+// var date = new Date();
+// console.log(date);
+// Kết quả dạng: Tue Jun 04 2024 23:10:05 GMT+0700 (Indochina Time)
+
+// var date = new Date(10000000000000);
+// console.log(date);
+// Kết quả dạng: Sun Nov 21 2286 00:46:40 GMT+0700 (Indochina Time)
+
+// var date = new Date("April 06, 2017 12:00:00");
+// console.log(date);
+// Kết quả dạng: Thu Apr 06 2017 12:00:00 GMT+0700 (Indochina Time)
+
+var date = new Date(2017,04,06,12,08,05,150);
+console.log(date);
+// Kết quả dạng: Sat May 06 2017 12:08:05 GMT+0700 (Indochina Time)
+
